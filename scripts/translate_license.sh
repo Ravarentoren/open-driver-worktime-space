ccf338e17aa262afc4c6779385e3d1fe10418499
Ravarentoren <222561548+Ravarentoren@users.noreply.github.com>
2026-01-02 18:35:11 +0100

    Add Apache License 2.0 and license translation generator

---
+++ scripts/translate_license.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCE_FILE="LICENSE"
+
+declare -A LANG_NAMES=(
+  [cs]="Čeština"
+  [de]="Deutsch"
+  [fr]="Français"
+  [ru]="Русский"
+)
+
+declare -A HUMAN_NOTICE=(
+  [cs]="> ℹ️ Toto je **informativní překlad** licence Apache 2.0.\n> \n> **Právně závazná je pouze anglická verze** v souboru `LICENSE`.\n> \n> V případě jakéhokoli rozporu má přednost anglické znění."
+  [de]="> ℹ️ Dies ist eine **informative Übersetzung** der Apache License 2.0.\n> \n> **Rechtlich verbindlich ist ausschließlich die englische Version** in der Datei `LICENSE`.\n> \n> Im Falle von Abweichungen gilt der englische Text."
+  [fr]="> ℹ️ Ceci est une **traduction informative** de la licence Apache 2.0.\n> \n> **Seule la version anglaise** du fichier `LICENSE` est juridiquement contraignante.\n> \n> En cas de divergence, le texte anglais prévaut."
+  [ru]="> ℹ️ Это **информационный перевод** лицензии Apache 2.0.\n> \n> **Юридически обязательной является только английская версия** в файле `LICENSE`.\n> \n> В случае расхождений приоритет имеет английский текст."
+)
+
+LEGAL_NOTICE_EN='> ⚠️ This is an **informative, non-authoritative translation** of the Apache License 2.0.
+>
+> The **only legally binding version** is the English text contained in the `LICENSE` file.
+>
+> In case of any discrepancy, the English version prevails.'
+
+for lang in "${!LANG_NAMES[@]}"; do
+  target="LICENSE.$lang.md"
+  echo "Generating $target (${LANG_NAMES[$lang]})"
+
+  cat <<EOF > "$target"
+$LEGAL_NOTICE_EN
+
+${HUMAN_NOTICE[$lang]}
+
+---
+
+$(cat "$SOURCE_FILE")
+EOF
+done
